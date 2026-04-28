8b8e4dca3eb695bea9cd8429ea10e392468fbf3c
Panagiotis Bernalis <pbernalis@live.com>
2026-04-28 17:28:45 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/intro">                    {/* ← Διορθώθηκε εδώ */}
+            to="/intro">
             🚀 Getting Started Guide
           </Link>
         </div>
