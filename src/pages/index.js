91b7b51a7a95fecb538c876771453a53171f9513
Panagiotis Bernalis <pbernalis@live.com>
2026-04-28 14:50:03 +0300

    Refactor homepage header and update button label
    Updated the homepage layout and modified the button text.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,11 @@ import clsx from 'clsx';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
@@ -16,10 +15,8 @@ function HomepageHeader() {
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Getting Started Guide 🚀
+          <Link className="button button--secondary button--lg" to="/docs/intro">
+            🚀 Getting Started Guide
           </Link>
         </div>
       </div>
@@ -28,15 +25,10 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
   return (
-    <Layout
-      title={`Welcome to ${siteConfig.title}`}
-      description="Centuply Help Center and Knowledge Base">
+    <Layout title="Centuply Help Center" description="Βοήθεια για το Centuply AI Website Builder">
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <main></main>
     </Layout>
   );
 }
